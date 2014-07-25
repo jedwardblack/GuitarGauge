b8eeeffc1f8c4ab57808ab986ed766f556ed62d9
jedwardblack <black.j.edward@gmail.com>
2014-07-25 13:15:18 -0400

    Initial Commit

---
+++ GuitarGauge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuitarGauge
+//
+//  Created by blackCloud on 7/25/14.
+//  Copyright (c) 2014 blackCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
